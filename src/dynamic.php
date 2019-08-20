48f4520133a6b3523051dd67f89c7f1b16751fef
JonDRiee <jonathan.de.riemaecker@continuous.team>
2019-08-20 14:32:38 +0200

    removing 2 variables

--- src/dynamic.php
+++ src/dynamic.php
@@ -1,10 +1,3 @@
 <?php
 
 echo "continuousphp " . (40 + 2);
-
-echo "\n\n --------------- \n\n";
-
-echo "BEHAT_BASE_URL: ", getenv('BEHAT_BASE_URL'), "\n";
-echo "BEHAT_WD_HOST: ", getenv('BEHAT_WD_HOST'), "\n";
-
-echo "phpversion: ", phpversion(), "\n";
